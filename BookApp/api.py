125139c4488e03cf3f20e6f6bb04b8e8b38d0964
bereket-7 <bekhonelign@gmail.com>
2024-10-26 23:27:49 +0300

    book model , fetch api

--- BookApp/api.py
+++ BookApp/api.py
@@ -1,26 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from BookApp.models import BookModel
+
 
 @api_view(['GET'])
 def BookListApi(request):
-    books = [
-        {
-            'name': 'The Alchemist',
-            'author': 'Paulo Coelho',
-        },
-        {
-            'name': '1984',
-            'author': 'George Orwell',
-        },
-        {
-            'name': 'To Kill a Mockingbird',
-            'author': 'Harper Lee',
-        },
-        {
-            'name': 'Moby Dick',
-            'author': 'Herman Melville',
-        }
+    books = BookModel.objects.all()
 
-    ]
+    books = [{
+        'name': book.name,
+        'author': book.author} for book in books]
     return Response(books)
