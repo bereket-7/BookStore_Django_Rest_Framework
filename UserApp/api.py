9ae81e84cd8abfcd402ce80efcf006f1ad9e066d
bereket-7 <bekhonelign@gmail.com>
2024-10-30 03:17:42 +0300

    protected api

--- UserApp/api.py
+++ UserApp/api.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['POST'])
@@ -27,3 +28,16 @@ def UserLoginApi(request):
         return Response({'message': 'Login successful', 'token': token.key}, status=status.HTTP_200_OK)
     else:
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+def ProtectedApi(request):
+    data = {
+        'message': 'This is a protected view accessible only with a valid token.',
+        'books': [
+            {'name': 'Book 1', 'author': 'Author A'},
+            {'name': 'Book 2', 'author': 'Author B'}
+        ]
+    }
+    return Response(data)
--- UserApp/urls.py
+++ UserApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api import UserCreateApi, UserLoginApi
+from .api import ProtectedApi, UserCreateApi, UserLoginApi
 
 urlpatterns = [
     path('register', UserCreateApi),
     path('login', UserLoginApi),
+    path('protected', ProtectedApi),
 ]
